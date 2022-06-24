58de15925187a623f64edc267b5daeca01c3cca2
Neto Rocha <63526723+NetoRocha97@users.noreply.github.com>
2022-06-24 02:58:56 -0300

    Iniciando Models

---
+++ models/estabelecimento.go
@@ -0,0 +1,12 @@
+package models
+
+type Estabelecimento struct {
+	ID          uint   `json:"id" gorm:"primaryKey"`
+	Nome        string `json:"nome"`
+	RazaoSocial string `json:"razao_social"`
+	Endereco    string `json:"endereco"`
+	Estado      string `json:"estado"`
+	Cidade      string `json:"cidade"`
+	Cep         int    `json:"cep"`
+	Numero      int    `json:"numero"`
+}
