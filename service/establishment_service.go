d48ec9f25ed47d6c7272306d658c0569bdbde8cc
Neto Rocha <63526723+NetoRocha97@users.noreply.github.com>
2022-08-21 15:27:41 -0300

    Atualização da camada service

--- service/establishment_service.go
+++ service/establishment_service.go
@@ -2,11 +2,13 @@ package service
 
 import (
 	// "modulo/controllers"
+	"fmt"
 	"modulo/database"
 	"modulo/models"
 	"modulo/view"
 
 	// "github.com/labstack/echo"
+
 	"gorm.io/gorm"
 )
 
@@ -20,7 +22,6 @@ func ShowEstablishmentService(id int) (view.ServiceEstablishment, error) {
 	var establishment models.Establishment
 	err := db.First(&establishment, id).Error
 
-
 	if err != nil {
 		return view.ServiceEstablishment{}, err
 	}
@@ -29,3 +30,63 @@ func ShowEstablishmentService(id int) (view.ServiceEstablishment, error) {
 
 }
 
+func ShowEstablishmentsService() ([]models.Establishment, error) {
+	var establishments []models.Establishment
+
+	db := database.GetDatabase()
+
+	err := db.Find(&establishments).Error
+
+	return establishments, err
+}
+
+func CreateEstablishmentService(establishment models.Establishment) error {
+
+	db := database.GetDatabase()
+
+	err := db.Create(&establishment).Error
+
+	return err
+
+}
+
+func UpdateEstablishmentService(updatedEstablishment models.Establishment) (models.Establishment, string) {
+
+	db := database.GetDatabase()
+
+	var optionalEstablishment models.Establishment
+	err := db.First(&optionalEstablishment, updatedEstablishment.ID).Error
+
+	errText := ""
+
+	if err != nil {
+		errText = fmt.Sprintf("O estabelecimento com ID %d não existe", updatedEstablishment.ID)
+	}
+
+	err = db.Save(&updatedEstablishment).Error
+
+	if err != nil {
+		errText = "Não foi possível atualizar o estabelecimento"
+	}
+
+	return optionalEstablishment, errText
+
+}
+
+func DeleteEstablishmentService(id int) string {
+	db := database.GetDatabase()
+
+	err := db.First(&models.Establishment{}, id).Error
+
+	if err != nil {
+		return "Não existe nenhum estabelecimento com este ID"
+	}
+
+	err = db.Delete(&models.Establishment{}, id).Error
+
+	if err != nil {
+		return "Não foi possível deletar o estabelecimento"
+	}
+
+	return ""
+}
